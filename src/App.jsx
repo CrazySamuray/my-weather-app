873c73188e9f7bf3b96e18463acb4fa9c246b411
Supreme Cadet <azulayidan1@gmail.com>
2024-03-25 15:26:13 +0200

    add basic search

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import './App.css'
 
 function App() {
 
+  const handleOnSearchChange = (searchData) =>  {
+    console.log(searchData)
+  }
+
   return (
-    <div className='App'>
-      <Search />
+    <div className='container'>
+      <Search  onSearchChange={handleOnSearchChange} />
     </div>
   )
 }
