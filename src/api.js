e951c411232266ea743d4ed95deb692156518646
Supreme Cadet <azulayidan1@gmail.com>
2024-03-25 15:57:41 +0200

    add api to connect to cities

---
+++ src/api.js
@@ -0,0 +1,17 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '78c99f5d4dmsh56243d39f263be9p19e43djsna9cb0f29689c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+try {
+	const response = await fetch(GEO_API_URL, geoApiOptions);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
